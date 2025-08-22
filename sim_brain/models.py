7e05368b8207062302bd1fe2dec5e611f5b712cd
AngusWhittaker <angus.whittaker2000@gmail.com>
2025-08-22 16:21:16 +1000

    x

--- sim_brain/models.py
+++ sim_brain/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 class Expert(models.Model):
-    name = models.CharField(max_length=100, null=False, blank=False)
+    name = models.CharField(max_length=100, null=False, blank=False, primary_key=True)
     prompt = models.TextField(null=False, blank=False)
